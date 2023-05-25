12719d7d4435e1abc127b21782e10db76f1a4349
Igor <igormurta7@gmail.com>
2023-05-25 16:21:18 -0300

    Construindo a estrutura e a sessão "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img
+      src="https://github.com/igormurta7.png"
+      alt="Foto de perfil no Github"
+    />
+    <Titulo fontSize={20}>Igor Murta</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,20 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi quia,
+      magni sit adipisci aut labore unde doloribus quidem aspernatur hic,
+      incidunt perferendis facere autem. Saepe amet nam consequatur nobis.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=igormurta7&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=igormurta7&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
